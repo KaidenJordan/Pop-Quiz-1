5eb35e89e55e9a4366375993b510565084e27a2c
KaidenJordan <69700580+KaidenJordan@users.noreply.github.com>
2020-09-21 11:05:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var wrapper = 
+document.body.querySelector(".wrapper");
+var number=5;
+var askedNumber=Number(prompt("Type in a number"));
+
+//this is where I got stuck
+if(askedNumber+number>10){
+  wrapper.innerHTML="Program is over";
+ }else{
+  //repeat=Number(prompt("Type in a number"));
+ }
